96ce61ef2edb959951e3f9546f7e73149b8afd8e
Koemy <francine.pns@gmail.com>
2022-08-01 15:50:47 -0300

    Learning how to create a component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Presentation from './components/Presentation';
 
 function App() {
     const banner = "https://cdnb.artstation.com/p/assets/images/images/043/873/357/large/danni-martin-velvet-a1.jpg?1638474674";
@@ -6,6 +7,7 @@ function App() {
         <div className="App">
             <h1>Welcome to the Velvet Room</h1>
             <img src={banner} alt="Velvet Room"/>
+            <Presentation/>
         </div>
     );
 }
---
+++ src/components/Presentation.js
@@ -0,0 +1,9 @@
+function Presentation() {
+    return(
+        <div>
+            <h2>Compendium</h2>
+        </div>
+    )
+}
+
+export default Presentation;
