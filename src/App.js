33061c3dd677b743ca32fe0078f2f1e54a0990f7
Koemy <francine.pns@gmail.com>
2022-08-03 18:03:47 -0300

    Modify index and app styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import Presentation from './components/Presentation';
 import Persona from './components/Persona';
+import Compendium from './components/Compendium';
 
 function App() {
-    const banner = "https://cdnb.artstation.com/p/assets/images/images/043/873/357/large/danni-martin-velvet-a1.jpg?1638474674";
     return (
-        <div className="app">
-            <h1>Welcome to the Velvet Room</h1>
-            <img src={banner} alt="Velvet Room"/>
+        <div>
             <Presentation/>
+            <Compendium/>
             <Persona personaImg = "https://static.wikia.nocookie.net/megamitensei/images/0/0b/Jack_Frost_SMTV_Art.png"
                      personaAlt= "Persona Jack Frost"
                      name="Jack Frost"
