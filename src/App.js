2096b3b77b2208dd6347ca98171f094cf1e21092
Koemy <francine.pns@gmail.com>
2022-08-04 15:46:42 -0300

    Include divider in app.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import './App.css';
 import Presentation from './components/Presentation';
 import Persona from './components/Persona';
 import Compendium from './components/Compendium';
+import Divider from './components/Divider';
 
 function App() {
     return (
         <div>
             <Presentation/>
+            <Divider/>
             <Compendium/>
             <Persona personaImg = "https://static.wikia.nocookie.net/megamitensei/images/0/0b/Jack_Frost_SMTV_Art.png"
-                     personaAlt= "Persona Jack Frost"
-                     name="Jack Frost"
-                     arcana="Magician"
-                     level="14"/>
+                     personaAlt= "Persona Jack Frost"/>
         </div>
     );
 }
