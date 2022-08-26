455f187e96f53c6ddcfa3a6072eb4a305a87a5e1
Koemy <francine.pns@gmail.com>
2022-08-26 16:07:25 -0300

    Change navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,17 @@
 import styles from '../css/NavBar.module.css'
 import {Link} from 'react-router-dom';
+import logo from '../img/logo.webp';
 
 function NavBar() {
     return (
         <nav id={styles["navBar"]}>
-            <Link to="/">Home</Link>
-            <Link to="/compendium">Compendium</Link>
+            <Link to="/">
+                <img src={logo} alt="Compendium Logo"/>
+            </Link>
+            <div>
+                <Link to="/">Home</Link>
+                <Link to="/compendium">Compendium</Link>
+            </div>
         </nav>
     )
 }
