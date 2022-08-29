39cfa6b71daf30d1432f8a6d0bb966edd12adcbc
Koemy <francine.pns@gmail.com>
2022-08-29 16:37:30 -0300

    Learn about fetch and useEffect

--- src/pages/Compendium.js
+++ src/pages/Compendium.js
@@ -2,8 +2,20 @@ import styles from '../css/Compendium.module.css';
 import Button from '../components/Button';
 import Input from '../components/Input';
 import Select from '../components/Select'
+import {useState, useEffect} from 'react';
 
 function Compendium() {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((resp => resp.json())).then((data => {setCategories(data)})).catch((err) => console.log(err))
+    }, [])
+
     return (
         <div id={styles["persona"]}>
             <div>
@@ -13,10 +25,10 @@ function Compendium() {
             </div>
             <form>
                 <div>
-                   <Input type="text" text="Name" name="name" placeholder="Persona's name"/>
+                    <Input type="text" text="Name" name="name" placeholder="Persona's name"/>
                 </div>
                 <div>
-                    <Select name="category" text="Select Arcana"/>
+                    <Select name="category" text="Select Arcana" options={categories}/>
                 </div>
                 <div>
                     <Input type="number" text="Level" name="level" placeholder="Persona's level"/>
