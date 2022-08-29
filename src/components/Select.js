7670bef127a907c4cc55f7358bca31c24219e17e
Koemy <francine.pns@gmail.com>
2022-08-29 16:36:39 -0300

    Change Select logic

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,11 +1,14 @@
 import styles from '../css/Select.module.css'
 
-function Select ({text, name}){
-    return(
+function Select({text, name, options}) {
+    return (
         <div id={styles["select"]}>
             <label htmlFor={name}>{text}: </label>
             <select name={name} id={name}>
                 <option>Select one Arcana</option>
+                {options.map((option) => (
+                    <option value={option.id} key={option.id}>{option.name}</option>
+                ))}
             </select>
         </div>
     )
