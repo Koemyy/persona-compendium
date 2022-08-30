2bbda20fdc9c4add761ba37429dd5be92b40392d
Koemy <francine.pns@gmail.com>
2022-08-30 16:00:11 -0300

    Change categories with arcanas

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,10 +1,10 @@
 import styles from '../css/Select.module.css'
 
-function Select({text, name, options}) {
+function Select({text, name, options, handleOnChange, value}) {
     return (
         <div id={styles["select"]}>
             <label htmlFor={name}>{text}: </label>
-            <select name={name} id={name}>
+            <select name={name} id={name} onChange={handleOnChange} value={value || ''}>
                 <option>Select one Arcana</option>
                 {options.map((option) => (
                     <option value={option.id} key={option.id}>{option.name}</option>
